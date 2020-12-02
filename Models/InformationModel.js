906e50fb0e7f491025f68b07e5d0b2082f8dd66a
Francisco Escobar <jfescobar18@hotmail.com>
2020-12-02 03:10:20 -0600

    Async fixed

--- Models/InformationModel.js
+++ Models/InformationModel.js
@@ -2,9 +2,9 @@ const HttpCodes = require("../Utils/HttpCodes");
 const ApiResponse = require("../Controllers/ApiResponse");
 const StateActions = require("../DatabaseActions/StateActions");
 
-exports.getStates = function (req, res) {
+exports.getStates = async function (req, res) {
     try {
-        let states = StateActions.getStates().catch(error => { throw error });
+        let states = await StateActions.getStates().catch(error => { throw error });
         return ApiResponse.send(HttpCodes.OK, res, states, null);
     } catch (error) {
         return ApiResponse.send(HttpCodes.BAD_REQUEST, res, error.message, null);
